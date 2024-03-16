3ad5b9474ad5a2d5d7776179f6bb2acc8df409ee
RiccardoGalbusera <93911514+RiccardoGalbusera@users.noreply.github.com>
2024-03-16 10:17:12 +0000

    take fee for partycle lottery

--- contracts/src/PartycleHook.sol
+++ contracts/src/PartycleHook.sol
@@ -9,9 +9,14 @@ import {PoolKey} from "v4-core/src/types/PoolKey.sol";
 import {PoolId, PoolIdLibrary} from "v4-core/src/types/PoolId.sol";
 import {BalanceDelta} from "v4-core/src/types/BalanceDelta.sol";
 import {IPartycle} from "./interfaces/IPartycle.sol";
+import {IERC20} from "../lib/v4-core/lib/openzeppelin-contracts/contracts/interfaces/IERC20.sol";
+import {Currency, CurrencyLibrary} from "../lib/v4-core/src/types/Currency.sol";
 
 contract PartycleHook is BaseHook {
     using PoolIdLibrary for PoolKey;
+    using CurrencyLibrary for Currency;
+
+    uint256 PARTYCLE_FEE = 500;
 
     IPartycle partycle;
 
@@ -39,6 +44,37 @@ contract PartycleHook is BaseHook {
     }
 
     // requires calldata to be an abi.encoded address of the user's wallet
+    function beforeSwap(
+        address,
+        PoolKey calldata key,
+        IPoolManager.SwapParams calldata swapParams,
+        bytes calldata data
+    ) external override returns (bytes4) {
+        // negative if exact input, positive if exact output
+        int256 amountSpecified = swapParams.amountSpecified;
+
+        // if amount specified < 0, then manage it in afterSwap
+        if (amountSpecified > 0) return BaseHook.beforeSwap.selector;
+
+        // here we are sure it's an exact input
+        bool isAmountInToken0 = swapParams.zeroForOne;
+        uint256 feeAmount = (uint256(-amountSpecified) * PARTYCLE_FEE) /
+            100_000;
+
+        IERC20 token = IERC20(
+            isAmountInToken0
+                ? address(uint160(key.currency0.toId()))
+                : address(uint160(key.currency1.toId()))
+        );
+        address swapper = abi.decode(data, (address));
+        token.transferFrom(swapper, address(this), feeAmount);
+
+        partycle.mintERC20(swapper, feeAmount);
+
+        return BaseHook.beforeSwap.selector;
+    }
+
+    // requires calldata to be an abi.encoded address of the user's wallet
     function afterSwap(
         address,
         PoolKey calldata key,
@@ -46,15 +82,30 @@ contract PartycleHook is BaseHook {
         BalanceDelta,
         bytes calldata data
     ) external override returns (bytes4) {
-        uint256 amount = swapParams.amountSpecified > 0
-            ? uint256(swapParams.amountSpecified)
-            : uint256(-swapParams.amountSpecified);
+        // negative if exact input, positive if exact output
+        int256 amountSpecified = swapParams.amountSpecified;
+
+        // if amount specified > 0, then it was managed in beforeSwap
+        if (amountSpecified < 0) return BaseHook.afterSwap.selector;
+
+        // here we are sure it's an exact input
+        bool isAmountInToken0 = swapParams.zeroForOne;
+        uint256 feeAmount = (uint256(amountSpecified) * PARTYCLE_FEE) / 100_000;
+
+        IERC20 token = IERC20(
+            isAmountInToken0
+                ? address(uint160(key.currency0.toId()))
+                : address(uint160(key.currency1.toId()))
+        );
         address swapper = abi.decode(data, (address));
-        partycle.mintERC20(swapper, amount);
+        token.transferFrom(swapper, address(this), feeAmount);
+
+        partycle.mintERC20(swapper, feeAmount);
 
         return BaseHook.afterSwap.selector;
     }
 
+    // requires calldata to be an abi.encoded address of the user's wallet
     function afterAddLiquidity(
         address,
         PoolKey calldata key,
