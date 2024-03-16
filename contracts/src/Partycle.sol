2ed20987e1659bf4192ab81a802e3c599e66bf98
RiccardoGalbusera <93911514+RiccardoGalbusera@users.noreply.github.com>
2024-03-16 00:44:27 +0000

    override erc721 transfer to allow scratching an nft

--- contracts/src/Partycle.sol
+++ contracts/src/Partycle.sol
@@ -69,6 +69,48 @@ contract Partycle is ERC404, Ownable {
         return descriptor.dataURI(tokenId, seeds[tokenId]);
     }
 
+    function scratch(uint256 tokenId) public {
+        erc721TransferFrom(msg.sender, address(0), tokenId);
+    }
+
+    /// @notice Function for ERC-721 transfers from.
+    /// @dev This function is recommended for ERC721 transfers.
+    function erc721TransferFrom(
+        address from_,
+        address to_,
+        uint256 id_
+    ) public override {
+        // Prevent minting tokens from 0x0.
+        if (from_ == address(0)) {
+            revert InvalidSender();
+        }
+
+        if (from_ != _getOwnerOf(id_)) {
+            revert Unauthorized();
+        }
+
+        // Check that the operator is either the sender or approved for the transfer.
+        if (
+            msg.sender != from_ &&
+            !isApprovedForAll[from_][msg.sender] &&
+            msg.sender != getApproved[id_]
+        ) {
+            revert Unauthorized();
+        }
+
+        // We only need to check ERC-721 transfer exempt status for the recipient
+        // since the sender being ERC-721 transfer exempt means they have already
+        // had their ERC-721s stripped away during the rebalancing process.
+        if (erc721TransferExempt(to_)) {
+            revert RecipientIsERC721TransferExempt();
+        }
+
+        // Transfer 1 * units ERC-20 and 1 ERC-721 token.
+        // ERC-721 transfer exemptions handled above. Can't make it to this point if either is transfer exempt.
+        _transferERC20(from_, to_, units);
+        _transferERC721(from_, to_, id_);
+    }
+
     function _retrieveOrMintERC721(address to_) internal virtual override {
         if (to_ == address(0)) {
             revert InvalidRecipient();
